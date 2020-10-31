ad2b2425420cd537921d42b0e2509c6b540f2f98
Kathy-Zeng <70964239+Kathy-Zeng@users.noreply.github.com>
2020-10-31 10:28:41 -0700

    Update DeepDishPizza.java

--- DeepDishPizza.java
+++ DeepDishPizza.java
@@ -1,7 +1,9 @@
 /* Kathy Zeng
  * 10/29/20
  * DeepDishPizza.java
- * Create own parameters
+ * Description: Create an own parameter from a variable. A variable
+ * has been declare from Pizza.java called toppings with a keyword "super". 
+   The constructor has been overloaded. This program is subclass.
 */
 public class DeepDishPizza extends Pizza
 {
